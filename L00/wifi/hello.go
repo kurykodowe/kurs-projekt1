458bf519c4fe70f6a67b5a71a943b9f910b5ca99
Tomasz Ptak <tomasz@ptak.net.pl>
2016-06-21 22:10:19 +0100

    wifi L00

---
+++ L00/wifi/hello.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	println("Siedzą se kury domowe w domu
+	println("Drapią z nudów po głowie")
+	println("To przecież robi się siedząc w domu z dzieckiem")
+	print("Każdy facet to powie")
+	println("Kawa nie dobra sąsiadka zajęta")
+	println("Obiad sam sie gotuje")
+	println("I taka kura nudząc się w domu")
+	println("Zaraz coś tu zmajstruje")
+	print("A jak sie zbierze tych kurek wiecej")
+	println("Plus kogut całkiem kumaty")
+	println("To nie ma bata że zaraz znajdą)
+	println("Bardzo ciekawe tematy")
+	println("I wymyśliły że miast w domu znudzone")
+	println("Patrzeć na garnek z obiadem")
+	println("To się nauczą PROGRAMOWANIA")
+	println("Zrobią to całym stadem")
+	println("I jak Fenix z popiołu tak one z pieleszy")
+	println("Domowych powstały do góry")
+	println("I zamiast domowych mamy kodowe")
+	println("Mądre, odważne kury")
+}
