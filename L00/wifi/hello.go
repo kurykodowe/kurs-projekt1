92bbc0ded8896b5f8c58bf2d2a33740dd5f41ed8
Tomasz Ptak <tomasz@ptak.net.pl>
2016-06-29 00:39:06 +0100

    PD00 wifi poprawione

--- L00/wifi/hello.go
+++ L00/wifi/hello.go
@@ -1,17 +1,17 @@
 package main
 
 func main() {
-	println("Siedzą se kury domowe w domu
+	println("Siedzą se kury domowe w domu")
 	println("Drapią z nudów po głowie")
 	println("To przecież robi się siedząc w domu z dzieckiem")
-	print("Każdy facet to powie")
+	println("Każdy facet to powie")
 	println("Kawa nie dobra sąsiadka zajęta")
 	println("Obiad sam sie gotuje")
 	println("I taka kura nudząc się w domu")
 	println("Zaraz coś tu zmajstruje")
-	print("A jak sie zbierze tych kurek wiecej")
+	println("A jak sie zbierze tych kurek wiecej")
 	println("Plus kogut całkiem kumaty")
-	println("To nie ma bata że zaraz znajdą)
+	println("To nie ma bata że zaraz znajdą")
 	println("Bardzo ciekawe tematy")
 	println("I wymyśliły że miast w domu znudzone")
 	println("Patrzeć na garnek z obiadem")
