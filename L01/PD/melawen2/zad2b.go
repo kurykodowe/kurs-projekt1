6e4e73361ccea317e7ba4d2cb377c6d8f82cdb71
Tomasz Ptak <tomasz@ptak.net.pl>
2016-06-29 00:40:34 +0100

    PD01 melawen 2 format

--- L01/PD/melawen2/zad2b.go
+++ L01/PD/melawen2/zad2b.go
@@ -6,9 +6,9 @@ func main() {
 	var level = 3
 	var hight = 3
 
-	for i:=1;i<=level;i++{
-		for j:=i;j<hight+i;j++{
-			for k:=1;k<=j;k++{
+	for i := 1; i <= level; i++ {
+		for j := i; j < hight + i; j++ {
+			for k := 1; k <= j; k++ {
 				fmt.Printf("@")
 			}
 			fmt.Printf("\n")
