52eacbbd76da6e9639c8b2f7911a1afe7ab87875
Tomasz Ptak <tomasz@ptak.net.pl>
2016-06-29 00:43:03 +0100

    PD01 kuraczubata format

--- L01/PD/kuraczubata/czubata_fizzbuzz.go
+++ L01/PD/kuraczubata/czubata_fizzbuzz.go
@@ -3,11 +3,16 @@ package main
 func main() {
 
 	for i := 1; i < 101; i = i + 1 {
-		if i % 3==0  {print("\n"+"Fizz");
-			if i % 5==0 {
-				print("Buzz")}
+		if i % 3 == 0 {
+			print("\n" + "Fizz");
+			if i % 5 == 0 {
+				print("Buzz")
+			}
 
-		} else if i % 5==0 {print("\n"+"Buzz")
-		} else {print("\n"); print(i)}
+		} else if i % 5 == 0 {
+			print("\n" + "Buzz")
+		} else {
+			print("\n"); print(i)
+		}
 	}
 }
