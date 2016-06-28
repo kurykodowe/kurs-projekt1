5558af210b3bbe1aae6c9535c825e69b663bf60a
Tomasz Ptak <tomasz@ptak.net.pl>
2016-06-29 00:38:13 +0100

    L01 kompilacja poprawione

--- L01/kompilacja/kompilacja.go
+++ L01/kompilacja/kompilacja.go
@@ -1,10 +1,12 @@
 package main
 
 func main() {
-	if (1=1) {
-	println("a)
+	if (1 == 1) {
+		println("a")
 	} else {
-println("a"
-for x:=2;x<10;x++ {
-println(X)
+		println("a")
+		for x := 2; x < 10; x++ {
+			println(x)
+		}
+	}
 }
