b89f8486c56b44979b9e930996d7729bb35379ac
Tomasz Ptak <tomasz@ptak.net.pl>
2016-06-29 00:34:15 +0100

    melawen poprawione zadanie trzecie

--- L01/PD/melawen/zad3.go
+++ L01/PD/melawen/zad3.go
@@ -3,22 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var a = 6
+	var rozmiar_wciecia = 6
 
 	for i := 1; i <= 3; i++ {
 		for j := i; j < 2 * i + 2; j++ {
-			for k := j + 1; k < 2 * i + a; k++ {
+			for k := j + 1; k < 2 * i + rozmiar_wciecia; k++ {
 				fmt.Printf(" ")
 			}
-			for l := 1; l < j + 1; l++ {
-				fmt.Printf("@")
-			}
-			for l := 1; l < j + 1; l++ {
+			for l := 1; l < 2 * j + 1; l++ {
 				fmt.Printf("@")
 			}
 
 			fmt.Printf("\n")
 		}
-		a = a - 2
+		rozmiar_wciecia = rozmiar_wciecia - 2
 	}
 }
