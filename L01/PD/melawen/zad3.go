3ea19c81ad6d29908fc83d68cac3a733f2b40095
Tomasz Ptak <tomasz@ptak.net.pl>
2016-06-29 00:30:10 +0100

    melawen formatowanie

--- L01/PD/melawen/zad3.go
+++ L01/PD/melawen/zad3.go
@@ -3,22 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var a  = 6
+	var a = 6
 
-	for i:=1;i<=3;i++{
-		for j:=i;j<2*i+2;j++{
-			for k:=j+1;k<2*i+a;k++{
+	for i := 1; i <= 3; i++ {
+		for j := i; j < 2 * i + 2; j++ {
+			for k := j + 1; k < 2 * i + a; k++ {
 				fmt.Printf(" ")
 			}
-			for l:=1;l<j+1;l++{
+			for l := 1; l < j + 1; l++ {
 				fmt.Printf("@")
 			}
-			for l:=1;l<j+1;l++{
+			for l := 1; l < j + 1; l++ {
 				fmt.Printf("@")
 			}
 
 			fmt.Printf("\n")
 		}
-		a=a-2
+		a = a - 2
 	}
 }
