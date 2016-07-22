cb7558302d55f7f2b8d171af2068a76529f5a79e
Tomasz Ptak <tomasz@ptak.net.pl>
2016-07-23 00:30:59 +0100

    czyszczenie formatowania

--- L01/PD/melawen2/3b.go
+++ L01/PD/melawen2/3b.go
@@ -11,12 +11,15 @@ func main() {
 
 	for module_index := 0; module_index < TREE_MODULES_AMOUNT; module_index++ {
 		var module_height = MODULE_HEIGHT_BASE + module_index
+
 		for line_index := 0; line_index < module_height; line_index++ {
 			var line_width = LINE_WIDTH_BASE + module_index + line_index
 			line_margin := LINE_MARGIN_BASE - module_index - line_index
+
 			for k := 0; k < line_margin; k++ {
 				fmt.Printf(" ")
 			}
+
 			for l := 0; l < 2 * line_width; l++ {
 				fmt.Printf("@")
 			}
