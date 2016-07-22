829df5c19bee323b11cd82e20c6b802688ffeae0
Tomasz Ptak <tomasz@ptak.net.pl>
2016-07-23 00:58:34 +0100

    aniaka initial

---
+++ L01/PD/aniaka/Fizz Buzz.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	total_bytes := 0
+
+	for i := 1; i < 100; i = i + 1 {
+		bytes, error := fmt.Printf("i\n")
+
+	if (i%3==0){
+		fmt.Printf("Fizz")
+	}else if (i%5==0) {
+		fmt.Printf("Buzz")
+
+
+}
