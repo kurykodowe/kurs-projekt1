a51d4c4a7d07982fcfdb8f3ba186f24126392dd9
Tomasz Ptak <tomasz@ptak.net.pl>
2016-07-23 02:20:52 +0100

    PD01 asia.asia poprawione

--- L01/PD/asia.asia/ex2_HW2_polchoinki.go
+++ L01/PD/asia.asia/ex2_HW2_polchoinki.go
@@ -10,10 +10,10 @@ package main
 import "fmt"
 
 func main() {
-	for k := 0; k < 5; k = k + 1 {
-		for j := 1; j < 4; j = j + 1 {
+	for k := 0; k < 3; k = k + 1 {
+		for j := 0; j < 3 + k; j = j + 1 {
 
-			for i := 1; i < j + k; i = i + 1 {
+			for i := 0; i < j + k + 1; i = i + 1 {
 				fmt.Printf("*")
 			}
 			fmt.Printf("\n")
--- L01/PD/asia.asia/ex3_HW2_cała choinka.go
+++ L01/PD/asia.asia/ex3_HW2_cała choinka.go
@@ -10,13 +10,13 @@ package main
 import "fmt"
 
 func main() {
-	for k := 0; k < 5; k = k + 1 {
-		for j := 1; j < 4; j = j + 1 {
+	for k := 0; k < 3; k = k + 1 {
+		for j := 0; j < 3 + k; j = j + 1 {
 
-			for l := 1; l < 8 - (j + k); l = l + 1 {
+			for l := 0; l < 6 - (j + k); l = l + 1 {
 				fmt.Printf(" ")
 			}
-			for i := 1; i < (j + k) * 2; i = i + 1 {
+			for i := 0; i < (j + k) * 2 + 1; i = i + 1 {
 				fmt.Printf("L")
 			}
 
