e09cbd7bd4ecf73fd3b58e259571f824b315d340
Tomasz Ptak <tomasz@ptak.net.pl>
2016-08-04 00:07:23 +0100

    L02 przyklady

--- L02/Lekcje/L02_operatory_arytmetyczne2.go
+++ L02/Lekcje/L02_operatory_arytmetyczne2.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("2 / 3 =", 2 - 3)
+	fmt.Println("2 / 3 =", 2 / 3)
 	fmt.Println("2. / 3 =", 2. / 3)
 	fmt.Println("(2 + .0) / 3 =", (2 + .0) / 3)
 }
