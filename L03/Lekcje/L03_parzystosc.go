5454b18f704622936d44759cc3778a1ba84efa35
Tomasz Ptak <tomasz@ptak.net.pl>
2016-08-08 23:22:48 +0100

    L03 sprawdzanie parametrow

--- L03/Lekcje/L03_parzystosc.go
+++ L03/Lekcje/L03_parzystosc.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	if (len(os.Args) != 2) {
+		fmt.Println("Brak drugiego argumentu")
+		os.Exit(1)
+	}
 	number, _ := strconv.Atoi(os.Args[1])
 	if (number % 2 == 0) {
 		fmt.Printf("%v: Liczba parzysta\n", number)
