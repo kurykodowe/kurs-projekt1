808ee2871b08a0b20a086fff6a8fa0d69b94e370
Tomasz Ptak <tomasz@ptak.net.pl>
2016-10-24 22:30:52 +0100

    L04 fix

--- L04/Lekcje/L04_tekst_8.go
+++ L04/Lekcje/L04_tekst_8.go
@@ -4,12 +4,7 @@ import "fmt"
 
 func main() {
 	text := "Mydło lubi zabawę"
-	printed_counter := 0
-	for _, char := range string([]rune(text)[:6]) {
+	for _, char := range string([]rune(text)[:5]) {
 		fmt.Printf("%c\n", char)
-		printed_counter += 1
-		if printed_counter >= 5 {
-			break
-		}
 	}
 }
